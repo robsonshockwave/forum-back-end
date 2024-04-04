9892366993b0dea2b302213010021f65e013466f
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-04-03 22:48:21 -0300

    getters & setters das entidades

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -15,11 +15,41 @@ export class Answer extends Entity<AnswerProps> {
     return this.props.content;
   }
 
+  get authorId() {
+    return this.props.authorId;
+  }
+
+  get questionId() {
+    return this.props.questionId;
+  }
+
+  get createdAt() {
+    return this.props.createdAt;
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt;
+  }
+
+  get excerpt() {
+    return this.content.substring(0, 120).trimEnd().concat('...');
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date();
+  }
+
+  set content(content: string) {
+    this.props.content = content;
+    this.touch();
+  }
+
   // a classe Entity já está fazendo isso
   //   constructor(props: AnswerProps, id?: string) {
   //     super(props, id);
   //   }
 
+  // faz o papel de construtor, não precisa instaciar a classe para utilizar o método
   static create(
     props: Optional<AnswerProps, 'createdAt'>,
     id?: UniqueEntityID
