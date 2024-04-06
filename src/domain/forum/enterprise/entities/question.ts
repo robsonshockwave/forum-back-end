6abf9ec212b2ef4709e69028419e2af83b5528ff
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-04-05 21:34:23 -0300

    factories de testes / criar questão

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -4,7 +4,7 @@ import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Optional } from '@/core/types/optional';
 import dayjs from 'dayjs';
 
-interface QuestionProps {
+export interface QuestionProps {
   title: string;
   content: string;
   authorId: UniqueEntityID;
