59aee652661ca402648c196f70ee1e5ee8a03b16
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-04-07 13:58:55 -0300

    caso de uso/teste - listar comentários da resposta

--- test/repositories/in-memory-answer-comments-repository.ts
+++ test/repositories/in-memory-answer-comments-repository.ts
@@ -1,3 +1,4 @@
+import { PaginationParams } from '@/core/repositories/pagination-params';
 import { AnswerCommentsRepository } from '@/domain/forum/application/repositories/answer-comments-repository';
 import { AnswerComment } from '@/domain/forum/enterprise/entities/answer-comment';
 
@@ -21,5 +22,13 @@ export class InMemoryAnswerCommentsRepository
     this.answerComments = this.answerComments.filter(
       (comment) => comment.id.toString() !== answerComment.id.toString()
     );
+  }
+
+  async findManyByAnswerId(answerId: string, { page }: PaginationParams) {
+    const answerComments = this.answerComments
+      .filter((item) => item.answerId.toString() === answerId)
+      .slice((page - 1) * 20, page * 20);
+
+    return answerComments;
   }
 }
