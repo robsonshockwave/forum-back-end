d4b19cf19709cdb2e89073738d11062e6bafc027
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-04-07 12:45:21 -0300

    entidade/classe base de comentários

--- src/domain/forum/enterprise/entities/question-comment.ts
+++ src/domain/forum/enterprise/entities/question-comment.ts
@@ -1,43 +1,14 @@
-import { Entity } from '@/core/entities/entities';
 import { UniqueEntityID } from '@/core/entities/unique-entity-id';
 import { Optional } from '@/core/types/optional';
+import { Comment, CommentProps } from './comment';
 
-export interface QuestionCommentProps {
-  content: string;
-  authorId: UniqueEntityID;
+export interface QuestionCommentProps extends CommentProps {
   questionId: UniqueEntityID;
-  createdAt: Date;
-  updatedAt?: Date;
 }
 
-export class QuestionComment extends Entity<QuestionCommentProps> {
-  get content() {
-    return this.props.content;
-  }
-
-  get authorId() {
-    return this.props.authorId;
-  }
-
+export class QuestionComment extends Comment<QuestionCommentProps> {
   get questionId() {
     return this.props.questionId;
-  }
-
-  get createdAt() {
-    return this.props.createdAt;
-  }
-
-  get updatedAt() {
-    return this.props.updatedAt;
-  }
-
-  private touch() {
-    this.props.updatedAt = new Date();
-  }
-
-  set content(content: string) {
-    this.props.content = content;
-    this.touch();
   }
 
   // faz o papel de construtor, não precisa instaciar a classe para utilizar o método
